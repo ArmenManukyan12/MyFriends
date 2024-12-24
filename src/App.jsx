85588cf5ec989cdd8c87634b72131123c749ab0f
Armen Manukyan <Armenmanukyan80@gmail.com>
2024-12-24 04:04:13 +0400

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React, { useReducer } from "react";
 import { Route, Routes } from "react-router-dom";
-import ROUTES from "./Routes";
+// import ROUTES from "./Routes";
 const App = () => {
   return (
     <div>
       <Routes>
-        <Route path={ROUTES}>
+        <Route >
         </Route>
       </Routes>
     </div>
