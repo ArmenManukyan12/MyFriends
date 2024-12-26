cf2465181f24799ffc6344f06efb25825d857985
Melo-Yeghyan <yeghyanmelik@gmail.com>
2024-12-26 12:20:50 +0400

    Fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import React, { useReducer } from "react";
 import { Route, Routes } from "react-router-dom";
 // import ROUTES from "./Routes";
+
 const App = () => {
   return (
     <div>
-      <Routes>
-        <Route >
-        </Route>
-      </Routes>
+      
     </div>
   );
 };
